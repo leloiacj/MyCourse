4af763425e27ce340df3ae1dd27f3afef24524a8
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-07-02 13:52:42 +0200

    Add Services and Random Courses

--- Models/Services/Application/CourseService.cs
+++ Models/Services/Application/CourseService.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MyCourse.Models.ViewModels;
+using MyCourse.Models.Enums;
 
 namespace MyCourse.Models.Services.Application
 {
     public class CourseService
     {
-    //metodo che deve tornare la lista dei corsi 
+        //metodo che deve tornare la lista dei corsi 
+        //per adesso creiamo un algoritmo che crea dei corsi random
+        //successivamente leggeremo i corsi da database
         public List<CourseViewModel> GetServices(){
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            var courseList = new List<CourseViewModel>();//oggetto che conterrà la lista dei corsi
+            var rand = new Random();
+            for (int i = 1; i <= 20; i++)//creao 20 corsi random
+            {
+                var price = Convert.ToDecimal(rand.NextDouble() * 10 + 10);
+                var course = new CourseViewModel//creo un oggetto di tipo CourseViewModel e lo popolo in maniera random
+                {
+                    Id = i,
+                    Title = $"Corso {i}",
+                    CurrentPrice = new Money(Currency.EUR, price),
+                    FullPrice = new Money(Currency.EUR, rand.NextDouble() > 0.5 ? price : price - 1),
+                    Author = "Nome cognome",
+                    Rating = rand.Next(10, 50) / 10.0,
+                    ImagePath = "/logo.svg"
+                };
+                courseList.Add(course);
+            }
+            return courseList;
         }
     }
 }
