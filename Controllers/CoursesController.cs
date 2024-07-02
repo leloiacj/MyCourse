21d9d5ca32af7ede23e9b09ada8f84968d5240f8
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-07-02 12:09:35 +0200

    Add services application and css

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyCourse.Models.Services.Application;
+using MyCourse.Models.ViewModels;
 
 
 
@@ -16,12 +18,14 @@ namespace MyCourse.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var courseService = new CourseService();//Il controller crea un oggetto del servizio aaplicativo che deve utilizzare
+            List<CourseViewModel> courses = courseService.GetServices();//recupero la lista dei corsi 
+            return View(courses);//passo l'oggetto contenente la lista dei corsi alla view per mostrare i dati
         }
 
         public IActionResult Detail()
         {
-             return View();
+            return View();
         }
     }
 }
