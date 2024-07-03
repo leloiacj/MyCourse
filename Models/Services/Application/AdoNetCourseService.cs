63c52138067b0fb1eae64567fd7ece062ea12087
leloiacj <166014920+leloiacj@users.noreply.github.com>
2024-07-03 17:38:43 +0200

    Prova

--- Models/Services/Application/AdoNetCourseService.cs
+++ Models/Services/Application/AdoNetCourseService.cs
@@ -13,17 +13,18 @@ namespace MyCourse.Models.Services.Application
         //Questo servizio applicativo utilizza l'interfaccia IDatabaseAccessor per accedere al database
         private readonly IDatabaseAccessor db;
 
-        public AdoNetCourseService(IDatabaseAccessor db){
+        public AdoNetCourseService(IDatabaseAccessor db)
+        {
             this.db = db;
         }
-
+        //
         //metodo che recupera la lista di tutti i corsi presenti nel database
         public List<CourseViewModel> GetCourses()
         {
             //query che verrà eseguita nel database
             string query = "SELECT Id, Title, ImagePath, Author,Rating, FullPrice_Amount, FullPrice_Currency, CurrentPrice_Amount, CurrentPrice_Currency FROM Courses";
             //un oggetto di tipo DataSet è un insieme di oggetti di tipo DataTable
-            DataSet dataSet = db.Query(query); 
+            DataSet dataSet = db.Query(query);
 
             //Un dataTable è una tabella in cui vengono memorizzati i dati recuperati da una SELECT nel db
             //dato che in un dataSet possono esserci più tabelle, con l'indice accedo all'i-esima tabella
@@ -31,7 +32,8 @@ namespace MyCourse.Models.Services.Application
             var courseList = new List<CourseViewModel>();
             //scorro l'oggetto DataTable riga per riga tramite la proprietà Rows
             //per ogni riga (oggetto DataRow) leggi i dati e crea l'oggetto CourseViewModel
-            foreach(DataRow courseRow in dataTable.Rows){
+            foreach (DataRow courseRow in dataTable.Rows)
+            {
                 var course = CourseViewModel.FromDataRow(courseRow);
                 courseList.Add(course);
             }
